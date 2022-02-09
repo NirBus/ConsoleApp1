3d222d1cb12022b7aa04f36f2f98495b8788f908
VenDiago <alizg@code.edu.az>
2022-02-09 08:26:02 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string txt = "lutfiyar";
+for (int i = 0; i < txt.Length; i++)
+{
+    if (txt[i] == 'a')
+    {
+        Console.WriteLine("a var");
+        break;
+    }
+    else if (i==txt.Length -1)
+    {
+        Console.WriteLine("a yoxdur");
+    }
+ 
+  
+}
+
+
